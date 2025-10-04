607a3c02c337f0d621a5e10f3c5acb6d61b3d283
Ayushmaan Gupta <ayush.261.gupta@gmail.com>
2025-10-05 02:40:21 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Cloud-Page/",
+  base: "./",
   server: {
     port: 3000,
     host: true
